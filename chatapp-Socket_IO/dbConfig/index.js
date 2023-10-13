3c7db3d7cdebc053251f4cd97ab3bfdb744e8157
Behrooz Khan <117090048+behroozkhan@users.noreply.github.com>
2023-10-13 21:34:36 +0500

    Connection with MongoDb

---
+++ chatapp-Socket_IO/dbConfig/index.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+import 'dotenv/config';
+
+mongoose.connect(`mongodb+srv://${process.env.DATABASE_NAME}:${process.env.DATABASE_PASSWORD}@ultratriotech.1nzsidg.mongodb.net/chatApp_Socket_io?retryWrites=true&w=majority`);
+
+
+export default mongoose;
