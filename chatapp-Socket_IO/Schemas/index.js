7b1811d781c5cfae3ef67855574a203dca526cba
Behrooz Khan <117090048+behroozkhan@users.noreply.github.com>
2023-10-13 21:40:09 +0500

    Joi Validation Schema For Checking Form Input

---
+++ chatapp-Socket_IO/Schemas/index.js
@@ -0,0 +1,42 @@
+import Joi from 'joi';
+
+const JoiValidSchema = Joi.object({
+    name: Joi.string()
+        .min(3)
+        .max(100)
+        .required()
+        .messages({
+            'string.min': 'Name should have at least {#limit} characters',
+            'string.max': 'Name should not exceed {#limit} characters',
+            'any.required': 'Name is required',
+        }),
+
+    password: Joi.string()
+        .min(8)
+        .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/)
+        .required()
+        .messages({
+            'string.min': 'Password should have at least {#limit} characters',
+            'string.pattern.base': 'Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character',
+            'any.required': 'Password is required',
+        }),
+
+    email: Joi.string()
+        .email({ minDomainSegments: 2 })
+        .required()
+        .messages({
+            'string.email': 'Email must be a valid email address',
+            'any.required': 'Email is required',
+        }),
+
+    phone: Joi.string()
+        .pattern(/^[0-9]{10}$/)
+        // .required()
+        .messages({
+            'string.pattern.base': 'Phone number must be a 10-digit numeric value',
+            'string.empty': 'Phone number is required',
+            'any.required': 'Phone number is required',
+        }),
+});
+
+export default JoiValidSchema;
